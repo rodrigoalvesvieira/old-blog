bc2b5da2c8c38a604f0d4b4a6fb7912d084d8430
Rodrigo Vieira <rodrigovieira1994@gmail.com>
2012-05-07 17:44:44 -0300

    stuff

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -1,2 +1,16 @@
-var blog = {
-};
+// Module RAV (for Rodrigo Alves Vieira)
+// where the blog's functions live
+
+var RAV = {
+  parseDate: function(dateStr) {
+    var dateArray = dateStr.split(" ");
+    dateArray[1] + " " + dateArray[0] + ", " + dateArray[2];
+  }
+}
+
+$(function(){
+  var dateText = $("time").text(),
+      realDate = RAV.parseDate(dateText);
+  
+  $("time").text(realDate);    
+});
